e93e0d6f43cd0ef7a15e0ba54acfa5b4c84ebc00
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-20 14:25:45 -0800

    dynamically add buttons for first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,41 @@ var questions = [];
 var startGame = function() {
     timer();
     question.textContent = "placeholder question";
+
+    // hides the `p` element and the start button 
+    var hiddenP = document.querySelector(".questionP");
+    hiddenP.setAttribute("class", "hide");
+    startButton.setAttribute("class", "hide")
+
+    // defines answer-choices div
+    var answerChoicesDiv = document.querySelector(".answer-choices");
+
+
     // button one
-    var optionOne = startButton;
-    console.log(optionOne);
+    var optionOne = document.createElement("button");
     optionOne.textContent = "Option One";
-    var hiddenEl = document.querySelector(".questionP");
-    hiddenEl.setAttribute("class", "hide");
-    console.log(hiddenEl);
+    optionOne.setAttribute("class", "btn");
 
     // button two
+    var optionTwo = document.createElement("button");
+    optionTwo.textContent = "Option Two";
+    optionTwo.setAttribute("class", "btn");
 
     // button three
+    var optionThree = document.createElement("button");
+    optionThree.textContent = "Option Three";
+    optionThree.setAttribute("class", "btn");
 
     // button four
+    var optionFour = document.createElement("button");
+    optionFour.textContent = "Option Four";
+    optionFour.setAttribute("class", "btn");
+
+    // adding buttons to answer-choices div
+    answerChoicesDiv.appendChild(optionOne);
+    answerChoicesDiv.appendChild(optionTwo);
+    answerChoicesDiv.appendChild(optionThree);
+    answerChoicesDiv.appendChild(optionFour);
    
 };
 
