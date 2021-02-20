3061f97adc923e4de37a45eb56a4652bdf03f6e3
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-20 13:59:07 -0800

    hides quiz instructions when questions appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
-var timeStart = 0;
-
+var startButton = document.querySelector(".btn");
+var question = document.querySelector("h1");
+var timeStart = 75;
+var questions = [];
 
 var startGame = function() {
-    
-}
+    timer();
+    question.textContent = "placeholder question";
+    // button one
+    var optionOne = startButton;
+    console.log(optionOne);
+    optionOne.textContent = "Option One";
+    var hiddenEl = document.querySelector(".questionP");
+    hiddenEl.setAttribute("class", "hide");
+    console.log(hiddenEl);
+
+    // button two
+
+    // button three
+
+    // button four
+   
+};
+
+var timer = function() {
+    console.log('time');
+};
+
+
+startButton.addEventListener("click", startGame);
