8f8fdfd6ff45aa0260b0535c3c48a9d51de5e723
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-21 16:28:26 -0800

    put the creation of dynamic js elements into the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,39 +24,43 @@ var answer;
 var startGame = function() {
     timer();
 
-    // hides the `p` element and the start button 
-    var hiddenP = document.querySelector(".questionP");
-    hiddenP.setAttribute("class", "hide");
-    startButton.setAttribute("class", "hide")
+    var createDynamicEl = function() {
+        // hides the `p` element and the start button 
+        var hiddenP = document.querySelector(".questionP");
+        hiddenP.setAttribute("class", "hide");
+        startButton.setAttribute("class", "hide")
 
-    // defines answer-choices div
-    var answerChoicesDiv = document.querySelector(".answer-choices");
+        // defines answer-choices div
+        var answerChoicesDiv = document.querySelector(".answer-choices");
 
-    // button one
-    var optionOne = document.createElement("button");
-    optionOne.textContent = "Option One";
-    optionOne.setAttribute("class", "btn");
+        // button one
+        var optionOne = document.createElement("button");
+        optionOne.textContent = "Option One";
+        optionOne.setAttribute("class", "btn");
 
-    // button two
-    var optionTwo = document.createElement("button");
-    optionTwo.textContent = "Option Two";
-    optionTwo.setAttribute("class", "btn");
+        // button two
+        var optionTwo = document.createElement("button");
+        optionTwo.textContent = "Option Two";
+        optionTwo.setAttribute("class", "btn");
 
-    // button three
-    var optionThree = document.createElement("button");
-    optionThree.textContent = "Option Three";
-    optionThree.setAttribute("class", "btn");
+        // button three
+        var optionThree = document.createElement("button");
+        optionThree.textContent = "Option Three";
+        optionThree.setAttribute("class", "btn");
 
-    // button four
-    var optionFour = document.createElement("button");
-    optionFour.textContent = "Option Four";
-    optionFour.setAttribute("class", "btn");
+        // button four
+        var optionFour = document.createElement("button");
+        optionFour.textContent = "Option Four";
+        optionFour.setAttribute("class", "btn");
 
-    // adding buttons to answer-choices div
-    answerChoicesDiv.appendChild(optionOne);
-    answerChoicesDiv.appendChild(optionTwo);
-    answerChoicesDiv.appendChild(optionThree);
-    answerChoicesDiv.appendChild(optionFour);
+        // adding buttons to answer-choices div
+        answerChoicesDiv.appendChild(optionOne);
+        answerChoicesDiv.appendChild(optionTwo);
+        answerChoicesDiv.appendChild(optionThree);
+        answerChoicesDiv.appendChild(optionFour);
+    };
+
+    createDynamicEl();
    
     // for loop to add questions to h1 el
     for(let i = 0; i < questions.length; i++) {
