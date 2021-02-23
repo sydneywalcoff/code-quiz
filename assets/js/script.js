3c4438335125e119f9ea9c09a14c4732e7c3ec37
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-23 11:57:43 -0800

    refactored button creation into functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var questions = [
         answerChoices: ["Batsheva", "Chana", "Shiriel", "Lior"]
     },
     {
-        question: "What is my dog's name",
+        question: "What is my dog's name?",
         answerChoices: ["Liba", "Sweetpea", "Lurch", "Sniffers"]
     },
     {
@@ -20,54 +20,102 @@ var questions = [
     }
 ];
 var answer;
+var idCounter=0;
+var answerChoicesDiv = document.querySelector(".answer-choices");
 
 var startGame = function() {
     timer();
 
+    hideEl();
+
+    firstQuestion();
+
     var createDynamicEl = function() {
-        // hides the `p` element and the start button 
-        var hiddenP = document.querySelector(".questionP");
-        hiddenP.setAttribute("class", "hide");
-        startButton.setAttribute("class", "hide")
 
-        // defines answer-choices div
-        var answerChoicesDiv = document.querySelector(".answer-choices");
 
-        // button one
-        var optionOne = document.createElement("button");
-        optionOne.textContent = "Option One";
-        optionOne.setAttribute("class", "btn");
+        // // button one
+        // var optionOne = document.createElement("button");
+        // optionOne.textContent = "Option One";
+        // optionOne.setAttribute("class", "btn");
+        // optionOne.setAttribute("id", idCounter);
 
-        // button two
-        var optionTwo = document.createElement("button");
-        optionTwo.textContent = "Option Two";
-        optionTwo.setAttribute("class", "btn");
+        // // button two
+        // var optionTwo = document.createElement("button");
+        // optionTwo.textContent = "Option Two";
+        // optionTwo.setAttribute("class", "btn");
 
-        // button three
-        var optionThree = document.createElement("button");
-        optionThree.textContent = "Option Three";
-        optionThree.setAttribute("class", "btn");
+        // // button three
+        // var optionThree = document.createElement("button");
+        // optionThree.textContent = "Option Three";
+        // optionThree.setAttribute("class", "btn");
 
-        // button four
-        var optionFour = document.createElement("button");
-        optionFour.textContent = "Option Four";
-        optionFour.setAttribute("class", "btn");
+        // // button four
+        // var optionFour = document.createElement("button");
+        // optionFour.textContent = "Option Four";
+        // optionFour.setAttribute("class", "btn");
 
-        // adding buttons to answer-choices div
-        answerChoicesDiv.appendChild(optionOne);
-        answerChoicesDiv.appendChild(optionTwo);
-        answerChoicesDiv.appendChild(optionThree);
-        answerChoicesDiv.appendChild(optionFour);
+        // // adding buttons to answer-choices div
+        // answerChoicesDiv.appendChild(optionOne);
+        // answerChoicesDiv.appendChild(optionTwo);
+        // answerChoicesDiv.appendChild(optionThree);
+        // answerChoicesDiv.appendChild(optionFour);
+        
+    
+
+        
     };
+   createDynamicEl();
 
-    createDynamicEl();
-   
-    // for loop to add questions to h1 el
-    for(let i = 0; i < questions.length; i++) {
-        question.textContent = questions[i].question;
-        console.log(question.textContent);
+    
+};
+
+var firstQuestion = function() {
+    var firstQItem = questions[0];
+    question.textContent = firstQItem.question;
+    for(let i = 0; i< questions[0].answerChoices.length; i++) {
+        createAnswerButtons(firstQItem.answerChoices[i]);
     }
 
+};
+
+var secondQuestion = function() {
+    var secondQItem = questions[1];
+    question.textContent = secondQItem.question;
+    for(let i = 0; i< questions[1].answerChoices.length; i++) {
+        createAnswerButtons(secondQItem.answerChoices[i]);
+    }
+};
+
+var thirdQuestion = function() {
+    var thirdQItem = questions[2];
+    question.textContent = thirdQItem.question;
+    for(let i = 0; i< questions[2].answerChoices.length; i++) {
+        createAnswerButtons(thirdQItem.answerChoices[i]);
+    }
+};
+
+var fourthQuestion = function() {
+    var fourthQItem = questions[3];
+    question.textContent = fourthQItem.question;
+    for(let i = 0; i< questions[3].answerChoices.length; i++) {
+        createAnswerButtons(fourthQItem.answerChoices[i]);
+    }
+};
+
+let createAnswerButtons = function(buttonTextIndex) {
+    var button = document.createElement("button");
+    button.textContent = buttonTextIndex;
+    button.setAttribute("class", "btn");
+    button.setAttribute("id", idCounter);
+    answerChoicesDiv.appendChild(button);
+    idCounter++;
+    console.log(idCounter);
+};
+
+var hideEl = function() {
+    var hiddenP = document.querySelector(".questionP");
+    hiddenP.setAttribute("class", "hide");
+    startButton.setAttribute("class", "hide")
 };
 
 var timer = function() {
